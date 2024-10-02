355e9e619fcdf04a50afc2b9af13da354ba903b3
murcoder <chris.murauer@yahoo.de>
2024-10-02 20:59:03 +0200

    hide floor

--- src/Models/Floor.jsx
+++ src/Models/Floor.jsx
@@ -1,9 +1,18 @@
 import { RigidBody } from '@react-three/rapier';
+import { useEffect, useRef } from 'react';
 
 export function Floor() {
+  const floor = useRef();
+  useEffect(() => {
+    // Do not render invisible walls (camera renders layer 0)
+    if (floor.current) {
+      floor.current.layers.set(1);
+    }
+  }, []);
+
   return <>
     <RigidBody type="fixed">
-      <mesh position-y={-1.25} receiveShadow={true}>
+      <mesh  ref={floor} position-y={-1.25} receiveShadow={true}>
         <boxGeometry args={[50, 0.5, 50]} />
         <meshStandardMaterial color="white" opacity={0} transparent={true}/>
       </mesh>
