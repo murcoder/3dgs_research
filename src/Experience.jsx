443ce3390c9761ed4446b496b8cda5239a9dbf93
murcoder <chris.murauer@yahoo.de>
2024-10-17 11:33:30 +0200

    refactoring

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,14 +2,14 @@ import { useControls } from 'leva';
 import { Perf } from 'r3f-perf';
 import React, { useRef, useState, useEffect } from 'react';
 import Room1 from './scenes/Room1.jsx';
-import LasercutDetail from './scenes/LasercutDetail.jsx';
 import Room2 from './scenes/Room2.jsx';
+import LasercutDetail from './scenes/LasercutDetail.jsx';
 import TechTest from './scenes/TechTest.jsx';
 
 export default function Experience() {
-  const scene1 = useRef();
-  const lasercutDetail = useRef();
+  const room1 = useRef();
   const room2 = useRef();
+  const lasercutDetail = useRef();
   const techTest = useRef();
 
   const { monitoring, debug, switchCameraControl, switchScenes } = useControls('world', {
@@ -17,7 +17,7 @@ export default function Experience() {
     switchScenes: {
       label: 'Scenes',
       options: { Room1: 1, Room2: 2, LasercutDetail: 3, TechTest: 4 },
-      value: 1
+      value: 3
     },
     switchCameraControl: {
       label: 'Camera',
@@ -45,7 +45,7 @@ export default function Experience() {
       {/*</GizmoHelper>*/}
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
-      {currentScene === 1 && (
+      {currentScene === 1 &&
         <Room1
           renderPriority={2}
           ref={room1}
@@ -54,10 +54,30 @@ export default function Experience() {
           laserCutterClicked={() => switchToScene(1)}
           doorClicked={() => switchToScene(2)}
         />
-      )}
-      {currentScene === 2 && <Room2  renderPriority={2} ref={room2} debug={debug} cameraMode={switchCameraControl} laserCutterClicked={switchToScene2} doorClicked={switchToScene1}/>}
-      {currentScene === 3 && <LasercutDetail  renderPriority={2} ref={lasercutDetail} debug={debug} cameraMode={switchCameraControl}/>}
-      {currentScene === 4 && <TechTest  renderPriority={2} ref={techTest} debug={debug} cameraMode={switchCameraControl}/>}
+      }
+      {currentScene === 2 &&
+        <Room2
+          renderPriority={2}
+          ref={room2}
+          debug={debug}
+          cameraMode={switchCameraControl}
+          laserCutterClicked={() => switchToScene(3)}
+          doorClicked={() => switchToScene(1)}/>
+      }
+      {currentScene === 3 &&
+        <LasercutDetail
+          renderPriority={2}
+          ref={lasercutDetail}
+          debug={debug}
+          cameraMode={switchCameraControl}/>
+      }
+      {currentScene === 4 &&
+        <TechTest
+          renderPriority={2}
+          ref={techTest}
+          debug={debug}
+          cameraMode={switchCameraControl}/>
+      }
     </>
   );
 }
