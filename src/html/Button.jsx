eadfa313e499c95b0f107d0f38beeaab2ba542a5
murcoder <chris.murauer@yahoo.de>
2024-10-29 11:59:04 +0100

    added translation

--- src/html/Button.jsx
+++ src/html/Button.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import {useTranslation} from 'react-i18next';
 
 export function Button({ handleClick }) {
-
+const {t} = useTranslation()
   return (
     <div>
       <button
         onClick={handleClick}
         className="z-50 absolute top-5 left-0 mt-16 ml-4 px-4 py-2 bg-black/80 text-white text-sm p-2 rounded hover:bg-black/60 transition-colors"
       >
-        Back
+        {t('buttons.back')}
       </button>
     </div>
   );
