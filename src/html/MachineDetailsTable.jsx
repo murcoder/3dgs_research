fb629e994d262c5da9db4ea29b26f0a7f2873b12
murcoder <chris.murauer@yahoo.de>
2024-11-13 14:17:02 +0100

    machine details translations

--- src/html/MachineDetailsTable.jsx
+++ src/html/MachineDetailsTable.jsx
@@ -1,25 +1,30 @@
+import { useTranslation } from 'react-i18next';
+
 export function MachineDetailsTable() {
+  const { t } = useTranslation();
+
   const machineDetails = [
-    { label: "Daten", value: "Speedy 100 flexx" },
-    { label: "Arbeitsfläche", value: "610mm x 305mm (Max Höhe: 132mm)" },
-    { label: "Ladefläche", value: "690mm x 346mm" },
-    { label: "Geschwindigkeit", value: "Max. 280cm/Sek. - CO2, Max. 200cm/Sek. - Faser" },
-    { label: "Beschleunigung", value: "4g" },
-    { label: "Motor", value: "Bürstenloser Gleichstrom-Servomotor" },
-    { label: "Encoder", value: "Inkrement Messsystem" },
-    { label: "Linsen", value: "CO2: 1,5“; 2,0“; 2,5“; 4,0“\nflexx: 2,85“\nFaser: 3,2“; 5.0“" },
-    { label: "Verfügbar", value: "flexx: 2,85“" },
-    { label: "Adressierbare Genauigkeit", value: "5µm" },
-    { label: "Wiederholgenauigkeit", value: "0,015mm" },
-    { label: "Teilemaßhaltigkeit", value: "Abhängig von Material und Arbeitsprozess" },
-    { label: "Max. Werkstückgewicht", value: "bis zu 10kg Belastung über gesamte Arbeitsfläche" },
-    { label: "Absaugung", value: "Flächenabsaugung und Tischabsaugung" },
-    { label: "Laserausstattung", value: "CO2:\nAbgeschlossener CO2 Laser, Luftgekühlt, 60W, 10,6µm\nFaser:\nGepulster Faserlaser, 30W, 1064nm fiber" },
-    { label: "Laserklasse", value: "CDRH Lasersicherheit, CE getestet, Laserklasse 2" },
-    { label: "Interlock (Verriegelung)", value: "Doppeltes Interlock Sicherheitssystem" },
-    { label: "Dimensionen", value: "1221mm x 830mm x 1055mm, Gewicht: 285kg" },
-    { label: "Strombedarf", value: "Ca. 1,2 bis 1,9kW (abhängig von Laserleistung)" },
+    { label: t("lasercutDetails.machineData"), value: "Speedy 100 flexx" },
+    { label: t("lasercutDetails.workArea"), value: "610mm x 305mm (Max Höhe: 132mm)" },
+    { label: t("lasercutDetails.loadingArea"), value: "690mm x 346mm" },
+    { label: t("lasercutDetails.speed"), value: "Max. 280cm/Sek. - CO2, Max. 200cm/Sek. - Faser" },
+    { label: t("lasercutDetails.acceleration"), value: "4g" },
+    { label: t("lasercutDetails.motor"), value: "Bürstenloser Gleichstrom-Servomotor" },
+    { label: t("lasercutDetails.encoder"), value: "Inkrement Messsystem" },
+    { label: t("lasercutDetails.lenses"), value: "CO2: 1,5“; 2,0“; 2,5“; 4,0“\nflexx: 2,85“\nFaser: 3,2“; 5.0“" },
+    { label: t("lasercutDetails.available"), value: "flexx: 2,85“" },
+    { label: t("lasercutDetails.addressableAccuracy"), value: "5µm" },
+    { label: t("lasercutDetails.repeatability"), value: "0,015mm" },
+    { label: t("lasercutDetails.partConsistency"), value: "Abhängig von Material und Arbeitsprozess" },
+    { label: t("lasercutDetails.maxWorkpieceWeight"), value: "bis zu 10kg Belastung über gesamte Arbeitsfläche" },
+    { label: t("lasercutDetails.extraction"), value: "Flächenabsaugung und Tischabsaugung" },
+    { label: t("lasercutDetails.laserEquipment"), value: "CO2:\nAbgeschlossener CO2 Laser, Luftgekühlt, 60W, 10,6µm\nFaser:\nGepulster Faserlaser, 30W, 1064nm fiber" },
+    { label: t("lasercutDetails.laserClass"), value: "CDRH Lasersicherheit, CE getestet, Laserklasse 2" },
+    { label: t("lasercutDetails.interlock"), value: "Doppeltes Interlock Sicherheitssystem" },
+    { label: t("lasercutDetails.dimensions"), value: "1221mm x 830mm x 1055mm, Gewicht: 285kg" },
+    { label: t("lasercutDetails.powerRequirement"), value: "Ca. 1,2 bis 1,9kW (abhängig von Laserleistung)" }
   ];
+
 
   return (
     <div className="w-[500px] overflow-hidden text-white text-xs rounded-lg bg-black/80  border border-white/50">
